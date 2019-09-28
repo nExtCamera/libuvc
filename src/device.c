a0e6921bae46bf0dcdfc9d26e830c328d78f6e80
Jakub Księżniak <jksiezni@users.noreply.github.com>
2019-09-28 13:51:10 +0200

    Revert "In some (as yet unidentified) circumstances it is possible for an interface to be claimed more than once.  This can cause an error."
    This reverts commit 231ecc8a

--- src/device.c
+++ src/device.c
@@ -355,6 +355,8 @@ static uvc_error_t uvc_open_internal(
   if (ret != UVC_SUCCESS)
     goto fail;
 
+  libusb_set_auto_detach_kernel_driver(usb_devh, 1);
+
   UVC_DEBUG("claiming control interface %d", internal_devh->info->ctrl_if.bInterfaceNumber);
   ret = uvc_claim_if(internal_devh, internal_devh->info->ctrl_if.bInterfaceNumber);
   if (ret != UVC_SUCCESS)
@@ -404,6 +406,7 @@ static uvc_error_t uvc_open_internal(
   if ( internal_devh->info ) {
     uvc_release_if(internal_devh, internal_devh->info->ctrl_if.bInterfaceNumber);
   }
+  libusb_set_auto_detach_kernel_driver(usb_devh, 0);
   libusb_close(usb_devh);
   uvc_unref_device(dev);
   uvc_free_devh(internal_devh);
@@ -971,32 +974,13 @@ void uvc_unref_device(uvc_device_t *dev) {
  * @param idx UVC interface index
  */
 uvc_error_t uvc_claim_if(uvc_device_handle_t *devh, int idx) {
-  int ret = UVC_SUCCESS;
+  int ret;
 
   UVC_ENTER();
-
-  if ( devh->claimed & ( 1 << idx )) {
-    UVC_DEBUG("attempt to claim already-claimed interface %d\n", idx );
-    UVC_EXIT(ret);
-    return ret;
-  }
-
-  /* Tell libusb to detach any active kernel drivers. libusb will keep track of whether
-   * it found a kernel driver for this interface. */
-  ret = libusb_detach_kernel_driver(devh->usb_devh, idx);
-
-  if (ret == UVC_SUCCESS || ret == LIBUSB_ERROR_NOT_FOUND || ret == LIBUSB_ERROR_NOT_SUPPORTED) {
-    UVC_DEBUG("claiming interface %d", idx);
-    if (!( ret = libusb_claim_interface(devh->usb_devh, idx))) {
-      devh->claimed |= ( 1 << idx );
-    }
-  } else {
-    UVC_DEBUG("not claiming interface %d: unable to detach kernel driver (%s)",
-              idx, uvc_strerror(ret));
-  }
-
+  UVC_DEBUG("claiming interface %d", idx);
+  ret = libusb_claim_interface(devh->usb_devh, idx);
   UVC_EXIT(ret);
-  return ret;
+  return (uvc_error_t) ret;
 }
 
 /** @internal
@@ -1007,39 +991,17 @@ uvc_error_t uvc_claim_if(uvc_device_handle_t *devh, int idx) {
  * @param idx UVC interface index
  */
 uvc_error_t uvc_release_if(uvc_device_handle_t *devh, int idx) {
-  int ret = UVC_SUCCESS;
+  int ret;
 
   UVC_ENTER();
   UVC_DEBUG("releasing interface %d", idx);
-  if (!( devh->claimed & ( 1 << idx ))) {
-    UVC_DEBUG("attempt to release unclaimed interface %d\n", idx );
-    UVC_EXIT(ret);
-    return ret;
-  }
-
   /* libusb_release_interface *should* reset the alternate setting to the first available,
      but sometimes (e.g. on Darwin) it doesn't. Thus, we do it explicitly here.
      This is needed to de-initialize certain cameras. */
-  libusb_set_interface_alt_setting(devh->usb_devh, idx, 0);
+  //libusb_set_interface_alt_setting(devh->usb_devh, idx, 0);
   ret = libusb_release_interface(devh->usb_devh, idx);
-
-  if (UVC_SUCCESS == ret) {
-    devh->claimed &= ~( 1 << idx );
-    /* Reattach any kernel drivers that were disabled when we claimed this interface */
-    ret = libusb_attach_kernel_driver(devh->usb_devh, idx);
-
-    if (ret == UVC_SUCCESS) {
-      UVC_DEBUG("reattached kernel driver to interface %d", idx);
-    } else if (ret == LIBUSB_ERROR_NOT_FOUND || ret == LIBUSB_ERROR_NOT_SUPPORTED) {
-      ret = UVC_SUCCESS;  /* NOT_FOUND and NOT_SUPPORTED are OK: nothing to do */
-    } else {
-      UVC_DEBUG("error reattaching kernel driver to interface %d: %s",
-                idx, uvc_strerror(ret));
-    }
-  }
-
   UVC_EXIT(ret);
-  return ret;
+  return (uvc_error_t) ret;
 }
 
 /** @internal
@@ -1726,6 +1688,8 @@ void uvc_close(uvc_device_handle_t *devh) {
     uvc_stop_streaming(devh);
 
   uvc_release_if(devh, devh->info->ctrl_if.bInterfaceNumber);
+
+  libusb_set_auto_detach_kernel_driver(devh->usb_devh, 0);
 
   /* If we are managing the libusb context and this is the last open device,
    * then we need to cancel the handler thread. When we call libusb_close,
